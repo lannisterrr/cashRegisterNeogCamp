77f58f6d3b9ae2c2c2d458051f9aa07368212c10
siddhant <siddhantp736@gmail.com>
2021-09-18 11:13:20 +0530

    completed style and js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let billAmount = document.getElementById('bill-amount');
-let cashAmount = document.getElementById('cash-amount');
+let cashAmount = document.getElementById('cash-given');
 let checkButton = document.getElementById('btn-1');
 let errorMessage = document.getElementById('error-message');
+let noOfNotes = document.querySelectorAll('.no-of-notes');
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 const validateAmount = () => {
   if (billAmount.value > 0) {
@@ -16,11 +19,19 @@ const validateAmount = () => {
   }
 };
 
+function calculateChange(amountReturned) {
+  for (let i = 0; i < availableNotes.length; i++) {
+    const numberOfNotes = Math.trunc(amountReturned / availableNotes[i]);
+    amountReturned %= availableNotes[i];
+    noOfNotes[i].innerText = numberOfNotes;
+  }
+}
+
 function hideMessage() {
   errorMessage.style.display = 'none';
 }
 
-function showMessage() {
+function showMessage(msg) {
   errorMessage.style.display = 'block';
   errorMessage.innerText = msg;
 }
